836428b9a35c781388387d6790d7ba76bd7e320c
Glory2Ukraine <vorobiova.yu@gmail.com>
2023-04-29 07:06:51 +0100

    Added the project folder structure

---
+++ src/script.js
@@ -0,0 +1,60 @@
+function formatDate(date) {
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[dayIndex];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+let currentTime = new Date();
+let currentDate = document.querySelector("#current-date");
+
+currentDate.innerHTML = formatDate(currentTime);
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#conditions").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "03c4b04fb969ec57b34fc911c55505ed";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", handleSubmit);
+
+search("Vancouver");
